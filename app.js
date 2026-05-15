14f38a9669e0ec6abd952bfc6af39aa060b3bb37
Surafel <surafelyigezuu@gmail.com>
2026-05-15 17:15:45 +0300

    creating a data for users,trips...

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.json());
+// app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello World');
+  res.status(200).json({
+    message: 'Hello World from the server',
+    app: 'ai-travel-assistant',
+  });
 });
 
 const port = 3000;
