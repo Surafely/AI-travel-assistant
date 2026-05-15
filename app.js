b01de1d7d160713a7d47a311044bba7c1602640e
Surafel <surafelyigezuu@gmail.com>
2026-05-15 15:20:27 +0300

    setting up the app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello World');
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}...`);
+});
