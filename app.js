61f86756b80b44aed7cd6663ddb1bb8433fe2922
Surafel <surafelyigezuu@gmail.com>
2026-05-15 18:20:11 +0300

    Responding to URL params

--- app.js
+++ app.js
@@ -1,14 +1,64 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-// app.use(express.json());
+app.use(express.json());
 
-app.get('/', (req, res) => {
+// app.get('/', (req, res) => {
+//   res.status(200).json({
+//     message: 'Hello World from the server',
+//     app: 'ai-travel-assistant',
+//   });
+// });
+
+const trips = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/trips-simple.json`),
+);
+
+app.get('/api/v1/trips', (req, res) => {
   res.status(200).json({
-    message: 'Hello World from the server',
-    app: 'ai-travel-assistant',
+    status: 'success',
+    results: trips.length,
+    data: {
+      trips,
+    },
   });
+});
+
+// app.get('/api/v1/trips/:id', (req, res) => {
+//   console.log(req.params);
+//   const id = req.params.id * 1;
+
+//   const trip = trips.find((el) => el.id === req.params);
+
+//   res.status(200).json({
+//     status: 'success',
+//     data: {
+//       trip,
+//     },
+//   });
+// });
+
+app.post('/api/v1/trips', (req, res) => {
+  // console.log(req.body);
+  const newId = trips[trips.length - 1].id + 1;
+  const newTrip = Object.assign({ id: newId }, req.body);
+
+  trips.push(newTrip);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/trips-simple.json`,
+    JSON.stringify(trips),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          trip: newTrip,
+        },
+      });
+    },
+  );
 });
 
 const port = 3000;
