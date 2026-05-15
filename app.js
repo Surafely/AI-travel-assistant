96c3b8532136ce00c6c4b6cf872d589648c19a39
Surafel <surafelyigezuu@gmail.com>
2026-05-15 19:13:55 +0300

    Handling CRUD requests

--- app.js
+++ app.js
@@ -26,19 +26,53 @@ app.get('/api/v1/trips', (req, res) => {
   });
 });
 
-// app.get('/api/v1/trips/:id', (req, res) => {
-//   console.log(req.params);
-//   const id = req.params.id * 1;
+app.get('/api/v1/trips/:id', (req, res) => {
+  console.log(req.params);
+  const id = req.params.id * 1;
+  const trip = trips.find((el) => el.id === id);
 
-//   const trip = trips.find((el) => el.id === req.params);
+  // if (id > trips.length)
+  if (!trip)
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
 
-//   res.status(200).json({
-//     status: 'success',
-//     data: {
-//       trip,
-//     },
-//   });
-// });
+  res.status(200).json({
+    status: 'success',
+    data: {
+      trip,
+    },
+  });
+});
+
+app.patch('/api/v1/trips/:id', (req, res) => {
+  if (req.params.id > trips.length)
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      trip: 'Update your Trips....',
+    },
+  });
+});
+
+app.delete('/api/v1/trips/:id', (req, res) => {
+  if (req.params.id > trips.length)
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
 
 app.post('/api/v1/trips', (req, res) => {
   // console.log(req.body);
