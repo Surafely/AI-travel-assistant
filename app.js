22c8de38bee9253338b75ea65891e27a92675374
Surafel <surafelyigezuu@gmail.com>
2026-05-17 15:37:08 +0300

    serving static files

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express();
 // MIDDLEWARES
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   req.requestedTime = new Date().toISOString();
