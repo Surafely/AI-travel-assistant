faba7f6fd3b84fd0f0da7a7bf4621e1971f227bd
Surafel <surafelyigezuu@gmail.com>
2026-05-19 13:45:28 +0300

    creating a model and testing it

--- server.js
+++ server.js
@@ -14,6 +14,50 @@ mongoose.connect(DB).then(() => {
   console.log('DB Connected Successfully !!!');
 });
 
+const tripSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A trip must have a name'],
+    unique: true,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A trip must have a price'],
+  },
+  duration: {
+    type: Number,
+    // required: [true, 'A trip must have a duration'],z
+  },
+  difficulty: {
+    type: String,
+    // required: [true, 'A trip must have a difficulty'],
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const Trip = mongoose.model('Trip', tripSchema);
+
+const testTrip = new Trip({
+  name: 'Test Trip 1',
+  price: 1000,
+});
+
+testTrip
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}...`);
