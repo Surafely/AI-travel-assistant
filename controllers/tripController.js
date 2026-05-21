624df17c824cc03d4df9664a052cf5dc8794caa5
Surafel <surafelyigezuu@gmail.com>
2026-05-21 22:02:37 +0300

    refactoring API features

--- controllers/tripController.js
+++ controllers/tripController.js
@@ -1,50 +1,17 @@
 const Trip = require('../models/tripModel');
+const APIFeatures = require('../utils/apiFeatures');
 
 exports.getAllTrips = async (req, res) => {
   try {
-    // FILTERING
-    const queryObj = { ...req.query };
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    excludedFields.forEach((el) => delete queryObj[el]);
+    //EXCUTE QUERY
+    const features = new APIFeatures(Trip.find(), req.query)
+      .filter()
+      .sort()
+      .limitFileds()
+      .paginate();
+    const trips = await features.query;
 
-    // ADVANCED FILTERING
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-
-    const parsedQuery = JSON.parse(queryStr);
-
-    let query = Trip.find(parsedQuery);
-
-    // SORTING
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort('-createdAt');
-    }
-
-    // FIELDS LIMITATION
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields);
-    } else {
-      query = query.select('-__v');
-    }
-
-    // PAGINATION
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-
-    query = query.skip(skip).limit(limit);
-
-    if (req.query.page) {
-      const numTrips = await Trip.countDocuments();
-      if (skip >= numTrips) throw new Error('This page does not exist');
-    }
-
-    const trips = await query;
-
+    //SEND RESPONSE
     res.status(200).json({
       status: 'success',
       results: trips.length,
