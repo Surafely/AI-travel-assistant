992986474c650918bb2e32cc17f4a1a4235022f7
Surafel <surafelyigezuu@gmail.com>
2026-05-21 08:50:24 +0300

    modeling the trip

--- models/tripModel.js
+++ models/tripModel.js
@@ -6,27 +6,85 @@ const tripSchema = new mongoose.Schema({
     type: String,
     required: [true, 'A trip must have a name'],
     unique: true,
+    trim: true,
   },
+
   price: {
     type: Number,
     required: [true, 'A trip must have a price'],
   },
+
+  destination: {
+    type: String,
+    required: [true, 'A trip must have a destination'],
+  },
+
+  budget: {
+    type: Number,
+    required: [true, 'A trip must have a budget'],
+  },
+
   duration: {
     type: Number,
-    // required: [true, 'A trip must have a duration'],z
+    required: [true, 'A trip must have a duration'],
   },
-  difficulty: {
-    type: String,
-    // required: [true, 'A trip must have a difficulty'],
-  },
+
   ratingsAverage: {
     type: Number,
     default: 4.5,
   },
-  //   ratingsQuantity: {
-  //     type: Number,
-  //     default: 0,
+
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
+  },
+
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A trip must have a group size'],
+  },
+
+  difficulty: {
+    type: String,
+    required: [true, 'A trip must have a difficulty'],
+  },
+
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+
+  imageCover: {
+    type: String,
+    required: [true, 'A trip must have an imagecover.'],
+  },
+
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+
+  startDates: [Date],
+
+  // itinerary: [
+  //   {
+  //     day: {
+  //       type: Number,
+  //       required: true,
+  //     },
+
+  //     activity: {
+  //       type: String,
+  //       required: true,
+  //     },
   //   },
+  // ],
 });
 
 const Trip = mongoose.model('Trip', tripSchema);
